7da681a552a787b233a7e371e1b07f37126ccfd9
Steven Ayvar <ayvarsteven@gmail.com>
2022-10-02 13:13:25 -0500

    defines player selection and connects buttons

---
+++ js-test.js
@@ -0,0 +1,18 @@
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+
+let playerSelection = undefined
+
+rock.addEventListener('click', function() {
+    playerSelection = 'rock'
+    console.log(playerSelection)
+})
+paper.addEventListener('click', function() {
+    playerSelection = 'paper'
+    console.log(playerSelection)
+})
+scissors.addEventListener('click', function() {
+    playerSelection = 'scissors'
+    console.log(playerSelection)
+})
