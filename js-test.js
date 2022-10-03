cf90f66d105092d64b0a055a97729d9b627cd769
Steven Ayvar <ayvarsteven@gmail.com>
2022-10-02 21:32:31 -0500

    adds tally function, tally variables and function calls on the buttons

--- js-test.js
+++ js-test.js
@@ -51,6 +51,11 @@ rock.addEventListener('click', function() {
         const winnerDisplay = document.createElement('p');
         winnerDisplay.textContent = `Winner: ${winner}`
         body.appendChild(winnerDisplay);
+
+        // --- add-to-tally ---
+
+        tally();
+        console.log(`Player Tally: ${playerTally} | Computer Tally: ${computerTally}`)
     })
 
 paper.addEventListener('click', function() {
@@ -97,7 +102,6 @@ scissors.addEventListener('click', function() {
         const computerSelectionDisplay = document.createElement('p');
         computerSelectionDisplay.textContent = `Computer Selection: ${computerSelection}`
         body.appendChild(computerSelectionDisplay);
-
         // --- display-winner ---
 
         const winnerDisplay = document.createElement('p');
@@ -136,6 +140,33 @@ function playRound(player, computer) {
   }
 }
 
+// ====================== ~ tally =========================
 
+let computerTally = 0;
+let playerTally = 0;
+
+function tally() {
+  if (
+    winner === `it's a tie! You both chose rock.` ||
+    winner === `It's a tie! You both chose paper.` ||
+    winner === `It's a tie! You both chose scissors.`
+  ) {
+    console.log(`*** TIE ***`);
+  } else if (
+    winner === `Player Wins! Paper beats rock.` ||
+    winner === `Player Wins! Scissors beats paper.` ||
+    winner === `Player Wins! Rock beats scissors.`
+  ) {
+    console.log(`*** PLAYER WIN ***`);
+    return playerTally++;
+  } else if (
+    winner === `Computer Wins! Rock beats scissors.` ||
+    winner === `Computer Wins! Paper beats rock.` ||
+    winner === `Computer Wins! Scissors beats paper.`
+  ) {
+    console.log(`*** COMPUTER WIN ***`);
+    return computerTally++;
+  }
+}
 
 // ====================== ~ title =========================
